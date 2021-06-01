3eef5a7e067d5f2e2c86da85940d4e9ad767e872
Kirill <kirill.petrov.centrale@gmail.com>
2021-06-01 17:20:26 +0200

    add grid into map section map + restaurants list container

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ const App = () => {
   return (
     <div className="App">
       <Header></Header>
-      <h1>Good Food</h1>
       <MapSection></MapSection>
     </div>
   );
--- src/components/ListRestaurants.js
+++ src/components/ListRestaurants.js
@@ -3,26 +3,36 @@ import style from '../assets/styles/list_restaurants.module.css';
 const ListRestaurants = () => {
     return (
         <div className={style.list_restaurants}>
-            <div>
-                <div>
-                    <span>Reasto 1</span>
-                    <span>Address: Lorem ipsum</span>
+            <div className={style.list_restaurants_items}>
+                <div className={style.list_restaurants_item}>
+                    <div className={style.restaurant_infos}>
+                        <span>Reasto 1</span>
+                        <span>Address: Lorem ipsum</span>
+                    </div>
                 </div>
-                <div>
-                    <span>Reasto 2</span>
-                    <span>Address: Lorem ipsum</span>
+                <div className={style.list_restaurants_item}>
+                    <div className={style.restaurant_infos}>
+                        <span>Reasto 2</span>
+                        <span>Address: Lorem ipsum</span>
+                    </div>
                 </div>
-                <div>
-                    <span>Reasto 3</span>
-                    <span>Address: Lorem ipsum</span>
+                <div className={style.list_restaurants_item}>
+                    <div className={style.restaurant_infos}>
+                        <span>Reasto 3</span>
+                        <span>Address: Lorem ipsum</span>
+                    </div>
                 </div>
-                <div>
-                    <span>Reasto 4</span>
-                    <span>Address: Lorem ipsum</span>
+                <div className={style.list_restaurants_item}>
+                    <div className={style.restaurant_infos}>
+                        <span>Reasto 4</span>
+                        <span>Address: Lorem ipsum</span>
+                    </div>
                 </div>
-                <div>
-                    <span>Reasto 5</span>
-                    <span>Address: Lorem ipsum</span>
+                <div className={style.list_restaurants_item}>
+                    <div className={style.restaurant_infos}>
+                        <span>Reasto 5</span>
+                        <span>Address: Lorem ipsum</span>
+                    </div>
                 </div>
             </div>
         </div>
