225af07701f22b708b6dc1434a550f7c3a0ee7aa
Kirill Petrov <kirill.petrov.centrale@gmail.com>
2021-08-17 17:15:47 +0200

    create detail restaurants using modal bootstrap for each restaurant

---
+++ src/components/DetailRestaurant.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import style from '../assets/styles/map_section.module.css';
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button';
+
+const DetailRestaurant = (selectedRestaurant) => {
+    console.log(selectedRestaurant.selectedRestaurant.restaurantName)
+    const [show, setShow] = useState(false);
+
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const listItems = 
+    <div key={selectedRestaurant.selectedRestaurant.restaurantName}>
+        <div>
+            <div>
+                <span>{selectedRestaurant.selectedRestaurant.restaurantName}</span>
+                <span>Address: {selectedRestaurant.selectedRestaurant.address}</span>
+                <span> Comment:
+                    {
+                        selectedRestaurant.selectedRestaurant.ratings.map(rating => {
+                            return rating.comment;
+                        })
+                    }
+                </span>
+            </div>
+        </div>
+    </div>
+ 
+    return (
+        <>
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            { listItems }
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    )
+}
+
+export default DetailRestaurant
--- src/components/ListRestaurants.js
+++ src/components/ListRestaurants.js
@@ -1,10 +1,14 @@
 import style from '../assets/styles/list_restaurants.module.css';
+import DetailRestaurant from '../components/DetailRestaurant';
 import Search from '../components/Search';
 
 const ListRestaurants = (sortedRestaurants) => {
+
 let arr = []
 
-const average = (array) => array.reduce((a, b) => a + b) / array.length;
+const average = (array) => 
+    array.reduce((a, b) => a + b) / array.length
+
 
     const listItems = sortedRestaurants.listRestaurants.map((restaurant, index) =>
     <div key={restaurant.restaurantName}>
@@ -12,23 +16,25 @@ const average = (array) => array.reduce((a, b) => a + b) / array.length;
             <div className={style.restaurant_infos}>
                 <span>{restaurant.restaurantName}</span>
                 <span>Address: {restaurant.address}</span>
-                { arr = [] }
+                { arr.length = 0 }
                 <span> Rating:
                     {
                         restaurant.ratings.map(rating => {
+                            
                             arr.push(rating.stars)
                         })
                     }
 
                     { average(arr) } 
-                    { arr = [] }
-                   </span>
+                    </span>
             </div>
+            <DetailRestaurant selectedRestaurant={restaurant}></DetailRestaurant>
         </div>
     </div>
 );
 
     return (
+        
         <div className={style.container_restaurants}>
             <Search></Search>
             <div className={style.list_restaurants_items}>
