5cb88f354f0528f2cc909acf5549f079d95c4356
Kirill <33541988+kirperov@users.noreply.github.com>
2021-11-29 16:36:39 +0100

    delete commented code, remove default logo

--- src/components/ReviewRestaurant.js
+++ src/components/ReviewRestaurant.js
@@ -1,15 +1,10 @@
 import StarRatings from "react-star-ratings";
 
 const ReviewRestaurant = (restaurantRating) => {
-  // const arr = []
-  // const average = (array) => array.reduce((a, b) => a + b) / array.length;
-
-  // restaurantRating.restaurantRating.map(rating => arr.push(rating.stars))
 
   return (
     <div>
       <StarRatings
-        // rating={average(arr)}
         rating={restaurantRating.restaurantRating}
         starRatedColor="blue"
         numberOfStars={5}
